0978132dae9958036939a2aa33f17523663c4831
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-04-30 15:07:24 -0700

    Update PostGenerator.js
    ready to pass data into the comment section

--- instagram/src/components/PostContainer/PostGenerator.js
+++ instagram/src/components/PostContainer/PostGenerator.js
@@ -1,13 +1,23 @@
 import React from 'react';
-// import PostHeader from './PostHeader.js';
+import CommentSection from '../CommentsSection'
 
 import './PostGenerator.css';
 
 const PostGenerator = props => {
 	return(
 		<div className='post'>
-		
+
+			<div className='header'>
+				<img alt='profile picture' className='profile-picture' src={props.postBanana.thumbnailUrl}/>
+				<div>{props.postBanana.username}</div>
+			</div>
+
+			<div className='image-container'>
+				<img alt='some image' src={props.postBanana.imageUrl} />
+			</div>
+			<CommentSection commentBanana={props.postBanana.commentsBanana} />
 		</div>
+
 	)
 }
 
